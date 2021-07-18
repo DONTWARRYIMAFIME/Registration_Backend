ae11a5361dd8c2207734a3c7a150f6d28e5ad427
Kostyukovich_V_A <demon4ik228228@gmail.com>
2021-07-18 19:15:32 +0300

    Initial commit

---
+++ src/test/java/by/spring/registration/Registration_Backend/RegistrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package by.spring.registration.Registration_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
